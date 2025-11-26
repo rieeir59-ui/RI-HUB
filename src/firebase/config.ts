b07116305cbbe5317c656f78eba4c45e5c25447f
RIE_EIR <rieeir59@gmail.com>
2025-11-26 05:36:09 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1477984206-e8ab3",
   "appId": "1:269476331162:web:dbf81b0cebe9cd2dad2923",
+  "storageBucket": "studio-1477984206-e8ab3.appspot.com",
   "apiKey": "AIzaSyDHYELDogt0c064L8Rd7nzBDUmg0EVnjuY",
   "authDomain": "studio-1477984206-e8ab3.firebaseapp.com",
   "measurementId": "",
